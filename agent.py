57d0e794d9b7ec59e3de233a676ef45d5ffa9b9a
Stephen <stephkno@gmail.com>
2020-01-25 23:58:44 -0800

    Update agent.py

--- agent.py
+++ agent.py
@@ -2,34 +2,26 @@ import torch
 
 #### PyTorch Actor Critic Model
 class Agent(torch.nn.Module):
-    def __init__(self, n):
+    def __init__(self, in_size=10, hidden=64, out=1):
         super(Agent, self).__init__()
 
-        self.actor = torch.nn.GRU(4, 64, 1)
-        self.critic = torch.nn.GRU(4, 64, 1)
-
-        self.actor_head = torch.nn.Sequential(
+        self.actor = torch.nn.Sequential(
+            torch.nn.Linear(in_size, hidden),
             torch.nn.Tanh(),
-            torch.nn.Linear(64, n, bias=False),
+            torch.nn.Linear(hidden, out),
             torch.nn.Softmax(dim=0)
         )
-        self.critic_head = torch.nn.Sequential(
+        self.critic = torch.nn.Sequential(
+            torch.nn.Linear(in_size, hidden),
             torch.nn.Tanh(),
-            torch.nn.Linear(64, 1, bias=False),
+            torch.nn.Linear(hidden, 1),
         )
-        self.actor_head[-2].weight.data.fill_(0.0)
-
-    def reset(self):
-        self.actor_hidden = torch.zeros(1,1,64)
-        self.critic_hidden = torch.zeros(1,1,64)
+        self.actor[-2].weight.data.fill_(0.0)
 
     def act(self, state):
         state = state.view(-1)
         #batch = state.shape[0]
 
-        action, self.actor_hidden = self.actor(state.float().unsqueeze(0).unsqueeze(0), self.actor_hidden)
-        values, self.critic_hidden = self.critic(state.float().unsqueeze(0).unsqueeze(0), self.critic_hidden)
-
-        action = self.actor_head(action.view(-1))
-        values = self.critic_head(values.view(-1))
+        action = self.actor(state.float().view(-1))
+        values = self.critic(state.float().view(-1))
         return action, values
