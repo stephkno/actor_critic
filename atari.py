c05d2721fc92d96bea37af679b72a5b2e8c500da
Stephen <stephkno@gmail.com>
2020-01-25 21:25:18 -0800

    Update atari.py

--- atari.py
+++ atari.py
@@ -1,11 +1,13 @@
+#!/Users/stephen/miniconda3/bin/python3
 import gym
 import torch
-import sys
-from collections import namedtuple
-import random
 import datetime
-
-env = gym.make("Breakout-ramNoFrameskip-v0")
+import random
+import sys
+from matplotlib import pyplot as plt
+from agent import Agent
+from memory import Memory
+from coach import Coach
 
 if len(sys.argv) > 1 and sys.argv[1].lower() == "test":
     test = True
@@ -19,95 +21,39 @@ def preprocess(state):
     #state = torch.tensor(state[[0x31, 0x36, 0x38, 0x3A, 0x3C]])/255.0
     state = torch.tensor(state)/255.0
     return state
+def learn(episode):
+    R = 0.0
+    log_probs = []
+    value_loss = []
+    returns = []
 
-#### PyTorch Actor Critic Model
-class Agent(torch.nn.Module):
-    def __init__(self):
-        super(Agent, self).__init__()
+    #state value calculation & preprocessing
+    for i,(_,_,reward) in enumerate(reversed(episode)):
+        R = reward + (GAMMA * R)
+        returns.insert(0,R)
 
-        self.actor = torch.nn.Sequential(
-            torch.nn.Linear(128, 128, bias=False),
-            torch.nn.Tanh(),
-            torch.nn.Linear(128, 64, bias=False),
-            torch.nn.Tanh(),
-            torch.nn.Linear(64, env.action_space.n, bias=False),
-            torch.nn.Softmax(dim=0)
-        )
-        self.critic = torch.nn.Sequential(
-            torch.nn.Linear(128, 128, bias=True),
-            torch.nn.Tanh(),
-            torch.nn.Linear(128, 64, bias=False),
-            torch.nn.Tanh(),
-            torch.nn.Linear(64, 1, bias=True),
-        )
-        self.actor[-2].weight.data.fill_(0.0)
+    returns = torch.tensor(returns)
+    mean = returns.mean()
+    std = returns.std() if returns.std() > 0 else 1
+    returns = (returns - mean)/std
 
-    def act(self, state):
-        #batch = state.shape[0]
-        action = self.actor(state.float()).view(-1)
-        values = self.critic(state.float()).view(-1)
-        return action, values
+    log_prob, values, _ = zip(*episode)
+    episode = list(zip(log_prob, values, returns))
 
-#tuple for episode transitions
-Transition = namedtuple('Transition', ('log_prob', 'state_value', 'reward'))
+    #calculate policy error
+    for i, (log_prob, state_value, returns) in enumerate(random.sample(episode,k=len(episode))):
+        advantage = returns - state_value
+        log_probs.append(-log_prob * advantage)
+        value_loss.append(0.5*(state_value - returns.unsqueeze(0)).pow(2))
 
-#replay memory for episodes
-class Memory():
-    def __init__(self):
-        super(Memory, self).__init__()
-        self.episode = []
-        self.reset()
-
-    def push(self, *args):
-        self.episode.append(Transition(*args))
-        self.length += 1
-
-    def sample(self):
-        return self.targets
-
-    def reset(self):
-        self.episode = []
-        self.length = 0
-
-    def learn(self):
-        R = 0.0
-        log_probs = []
-        value_loss = []
-        returns = []
-
-        #state value calculation & preprocessing
-        for i,(_,_,reward) in enumerate(reversed(self.episode)):
-            if reward < 0:
-                R = -1.0
-            elif reward > 0:
-                R = 1.0
-            else:
-                R = reward + (GAMMA * R)
-            returns.insert(0,R)
-
-        returns = torch.tensor(returns)
-        mean = returns.mean()
-        std = returns.std() if returns.std() > 0 else 1
-        returns = (returns - mean)/std
-
-        log_prob, values, _ = zip(*self.episode)
-        self.episode = list(zip(log_prob, values, returns))
-
-        #calculate policy error
-        for i, (log_prob, state_value, returns) in enumerate(random.sample(self.episode,k=len(self.episode))):
-            advantage = returns - state_value
-            log_probs.append(-log_prob * advantage)
-            value_loss.append((state_value - returns.unsqueeze(0)).pow(2))
-
-        #update parameters
-        print("Updating parameters")
-        optimizer.zero_grad()
-        loss = torch.cat((torch.cat(log_probs), torch.cat(value_loss))).mean()
-        loss.backward()
-        print("Loss: {}".format(loss.item()))
-        optimizer.step()
-        self.reset()
-
+    #update parameters
+    print("Updating parameters")
+    optimizer.zero_grad()
+    loss = torch.cat((torch.cat(log_probs), torch.cat(value_loss))).mean()
+    loss.backward()
+    print("Loss: {}".format(loss.item()))
+    optimizer.step()
+    memory.reset()
 #loading/saving checkpoint for testing
 def load_agent():
     print("Loading agent")
@@ -118,7 +64,7 @@ def save_model(model):
 
 #hyperparameters
 step = 0
-highest = -9999 #high score
+highest = -9999
 total_score = 0.0
 total_steps = 0
 batch_size = 10000
@@ -129,83 +75,44 @@ epochs = 1000
 episode = 1
 init_action = 1
 GAMMA = 0.9
-EPSILON = 0.2
-rate = 0.0002
+EPSILON = 1.0
+rate = 0.001
 UPDATE_INTERVAL = 1
+TARGET_UPDATE_INTERVAL = 10
+running_scores = []
+plot = True
+
+env_name = "CartPole-v0"
+env = gym.make(env_name)
 
 #create objects
 memory = Memory()
-agent = Agent()
+agent = Agent(env.action_space.n)
+coach = Coach()
+
 optimizer = torch.optim.Adam(params=agent.parameters(), lr=rate)
 print(agent)
 
 #training loop
 while True:
-    done = False
 
-    score = 0.0
-    confidence = 1.0
-
-    #reset game
-    state = env.reset()
-    state = preprocess(state)
-
-    env._max_episode_steps = 5000
-    render = test
-
-    if test:
-        load_agent()
-
-    #initial framekskip to get lives
-    for i in range(init_frameskip):
-        _, _, _, info = env.step(init_action)
-        if 'ale.lives' in info:
-            lives = info["ale.lives"]
-
-    #episode loop
-    while not done:
-        action_probs, state_value = agent.act(state)
-        dist = torch.distributions.Categorical(action_probs)
-        r = random.random()
-
-        if r < EPSILON and not test:
-            action = dist.sample()
-        else:
-            action = torch.argmax(action_probs)
-        next_state, reward, done, info = env.step(int(action))
-        next_state = preprocess(next_state)
-	
-        #check if lost atari life
-        if 'ale.lives' in info and info["ale.lives"] < lives:
-            lives = info["ale.lives"]
-            reward = -1.0
-
-        score += reward
-        total_score += reward
-        step += 1
-        total_steps += 1
-
-        if done:
-            reward = -1.0
-
-        #render env for observing agent
-        if render:
-            env.render()
-
-        #prime next state
-        state = next_state
-        memory.push(dist.log_prob(action), state_value, reward)
-
+    memory, score, steps = coach.run_episode(agent, env, memory, preprocess, test)
+    total_score += score
+    total_steps += steps
 
     print("[Episode {} Score:{}]".format(episode, score))
+    running_scores.append(score)
 
     if not test and episode % UPDATE_INTERVAL == 0:
-        memory.learn()
+        learn(memory.episode)
 
         if score > highest:
             save_model(agent)
             highest = score
-
+        if plot:
+            plt.plot(range(episode), running_scores)
+            plt.draw()
+            plt.pause(0.0000001)
         #display episode score
         print("[Episode {}|Step {} Score:{} High:{} Time:{}]".format(episode, total_steps, total_score, highest, datetime.datetime.now()))
         total_score = 0
